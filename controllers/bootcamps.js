f16c469d7e62631f1ba9ab70a5af8b4a50d12b9f
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-04-30 09:29:47 +0545

    Advanced results

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,6 +2,7 @@ const ErrorResponse = require("../utils/errorResponse")
 const asyncHandler = require("../middleware/async")
 const Bootcamp = require("../models/bootcamp")
 const path = require("path")
+const advancedResults = require("../middleware/advancedResult")
 
 /**
  * 
@@ -11,84 +12,10 @@ const path = require("path")
  */
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
-    let query;
-
-    // copy req.query 
-    const reqQuery = { ...req.query }
-
-    // Fields to exclude
-    const removeFields = ['select', 'sort', 'limit'];
-
-    // Loop over removeFields and delete them from reqQuery
-    removeFields.forEach(param => delete reqQuery[param])
+   
 
 
-    // to manipulate query we have to first convert to string
-    let queryStr = JSON.stringify(reqQuery)
-
-    // converting the query sting to $gt or $gte...
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
-
-
-
-    // Finding resource
-    query = Bootcamp.find(JSON.parse(queryStr)).populate({ path: 'courses', select: "title description" })
-
-
-
-
-    // select fields
-    if (req.query.select) {
-        const fields = req.query.select.split(",").join(' ')
-        query = query.select(fields)
-    }
-
-    // Sort
-    if (req.query.sort) {
-        const sortBy = req.query.sort.split(",").join(' ')
-        query = query.sort(sortBy)
-
-    } else {
-        query = query.sort("-createdAt") // - means descending
-    }
-
-
-    // pagination
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 100
-
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const total = await Bootcamp.countDocuments()
-
-    query = query.skip(startIndex).limit(limit)
-
-    // Executing query
-    const bootcamps = await query
-
-    // pagination result
-    const pagination = {}
-
-    if (endIndex < total) {
-        pagination.next = {
-            page: page + 1,
-            limit,
-        }
-    }
-    if (startIndex > 0) {
-        pagination.prev = {
-            page: page - 1,
-            limit,
-        }
-    }
-
-
-    res.status(200).json({
-        success: true,
-        // count: total,
-        pagination,
-        data: bootcamps
-    })
+    res.status(200).json(res.advancedResults)
 
 }
 )
