2bf425ab67c21b3337efe1afc0c0c2f88e0249ce
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-05-29 09:57:47 +0545

    advance Results

--- routes/bootcampRoutes.js
+++ routes/bootcampRoutes.js
@@ -7,20 +7,23 @@ const {
   getBootcamp,
   bootcampPhotoUpload,
 } = require("../controllers/bootcampController");
+const Bootcamp = require("../models/bootcamp")
 const router = express.Router();
 
 const courseRouter = require("./coursesRoutes");
 
+// const advancedResults = require("../middleware/advanceResult");
+ 
 //Re-route into other resource routes
 router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/").get(getBootcamps).post(createBootcamp);
 
 router.route("/:id/photo").put(bootcampPhotoUpload);
+// router.route.get(advancedResults(Bootcamp, "bootcamp"), getBootcamp)
 
 router
   .route("/:id")
-  .get(getBootcamp)
   .put(updateBootcamp)
   .delete(deleteBootcamp);
 
